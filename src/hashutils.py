5c7d2e7003ef3eca509d1f35e900a77f9413197b
qlrd <106913782+qlrd@users.noreply.github.com>
2023-09-30 14:08:07 -0300

    Added FileChooser popup to choose a file to be hashed
    modularized methods into properly classes

--- src/hashutils.py
+++ src/hashutils.py
@@ -33,13 +33,13 @@ def open_and_hash_file(**kwargs) -> str:
             _bytes = f_to_be_sig.read()  # read file as bytes
 
             if verbose:
-                verbose_log(f"Read bytes: {_bytes}")
+                verbose_log('INFO', f"Read bytes: {_bytes}")
 
             __readable_hash__ = hashlib.sha256(_bytes).hexdigest()
 
             # Prints the hash of the file
             if verbose:
-                verbose_log(f"Hash of {__filename__}: {__readable_hash__}")
+                verbose_log('INFO', f"Hash of {__filename__}: {__readable_hash__}")
 
             return __readable_hash__
     except FileNotFoundError as exc:
--- src/logutils.py
+++ src/logutils.py
@@ -4,14 +4,22 @@ logutils.py
 simple log utilities
 """
 import time
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    INFO = '\033[92m'
+    WARN = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def now() -> str:
     """Return some formated time"""
     return time.strftime("%X %x %Z")
 
 
-def verbose_log(v_data: str):
+def verbose_log(v_type: str, v_data: str):
     """
     Prints verbose data preceded by current time
 
@@ -20,11 +28,20 @@ def verbose_log(v_data: str):
     ```bash
     >>> import logutils
     >>> verbose_log('Hello world')
-    [08:08:08 8/8/88 -08] Hello World
+    [ksigner 08:08:08 8/8/88 -08] Hello World
 
     ```
     Args:
         :param v_data
             the message to be verbose
     """
-    print(f"[{now()}] {v_data}")
+    if (v_type == 'INFO'):
+        c = f"{bcolors.BOLD}{bcolors.INFO}"
+    
+    if (v_type == 'WARN'):
+        c = f"{bcolors.BOLD}{bcolors.WARN}"
+    
+    if (v_type == 'FAIL'):
+        c = f"{bcolors.BOLD}{bcolors.FAIL}"
+
+    print(f"[{c}{v_type}{bcolors.ENDC}   ] [KSigner     ] [{now()}] {v_data}")
